b8f9ad6ed622bd87e9a5714706660b4a4c9984de
Phuc <phuc.nguyen@webtretho.com>
2023-11-13 11:45:32 +0700

    update

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -24,12 +24,12 @@ end
 require("nvim-tree").setup({
   diagnostics = {
     enable = true,
-    show_on_dirs = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
+    show_on_dirs = false,
+    show_on_open_dirs = true,
+    -- debounce_delay = 50,
+    severity = {
+      min = vim.diagnostic.severity.ERROR,
+      max = vim.diagnostic.severity.ERROR,
     },
   },
 	on_attach = my_on_attach,
--- lua/phuc/remap.lua
+++ lua/phuc/remap.lua
@@ -29,6 +29,7 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.cursorline = true
+vim.opt.autoindent = true
 
 -- Primeagen remap
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
